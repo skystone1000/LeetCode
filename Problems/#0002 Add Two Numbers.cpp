c5efa5cc9182425dee42a344233b2e296590180f
Aditya <adityaspmahajan@gmail.com>
2021-11-09 12:12:07 +0530

    02 Add two Numbers

--- Problems/#0002 Add Two Numbers.cpp
+++ Problems/#0002 Add Two Numbers.cpp
@@ -3,6 +3,9 @@
 // 06 #0002 Add Two Numbers
 // https://leetcode.com/problems/add-two-numbers/
 
+#include<bits/stdc++.h>
+using namespace std;
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -20,31 +23,57 @@ struct ListNode{
         val = x;
         next = nullptr;
     }
-}
+};
 
-class Solution{
+// Iterative
+class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* headA, ListNode* headB){
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         // reverse the LL if the number are not.
-
         ListNode dummy(-1);
-        ListNode* curr = &dummy; 
-
+        ListNode* curr = &dummy;
+        
         int carry = 0;
-        while(headA || headB || carry){
-            int a = headA != NULL ? headA->val : 0;
-            int b = headB != NULL ? headB->val : 0;
-
+        while(l1 || l2 || carry){
+            int a=0,b=0;
+            if(l1) a = l1->val; // Same as checking if(l1!=NULL)
+            if(l2) b = l2->val;
+            
             int sum = a + b + carry;
-
-            curr->next = new ListNode(sum % 10); 
-            int carry = sum / 10;
-
-            headA = headA != NULL ? headA->next : NULL;
-            headB = headB != NULL ? headB->next : NULL;
+            curr->next = new ListNode(sum % 10);
+            carry = sum / 10;
+            
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
             curr = curr->next;
         }
-
         return dummy.next;
     }
+};
+
+// Recursive solution
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		return helper(l1, l2, 0);
+	}
+
+	ListNode* helper(ListNode* l1, ListNode* l2, int carry) {
+		if (!l1 && !l2 && !carry) {
+			return NULL;
+		}
+        
+        int a=0,b=0;
+        if(l1) a = l1->val; // Same as checking if(l1!=NULL)
+        if(l2) b = l2->val;
+        
+		int sum = a + b + carry;
+		ListNode* curr = new ListNode(sum % 10);
+        carry = sum / 10;
+        
+        if(l1) l1 = l1->next;
+        if(l2) l2 = l2->next;
+		curr->next = helper(l1, l2, carry);
+		return curr;
+	}
 };
