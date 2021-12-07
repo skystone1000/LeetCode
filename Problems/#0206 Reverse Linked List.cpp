209b6597710aa6592ccc68b83b93930f31ce583f
Aditya <adityaspmahajan@gmail.com>
2021-12-07 21:18:00 +0530

    1290 Convert Binary number to integer

--- Problems/#0206 Reverse Linked List.cpp
+++ Problems/#0206 Reverse Linked List.cpp
@@ -13,6 +13,14 @@
  * };
  */
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
 // Stack solution remaining
 
 
---
+++ Problems/#1290. Convert Binary Number in a Linked List to Integer.cpp
@@ -0,0 +1,45 @@
+// #1290. Convert Binary Number in a Linked List to Integer
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        // Reversing the LL
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        
+        while(curr!=NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        // Updating head;
+        head = prev;
+        
+        // Calculating the value
+        int multiplier = 1;
+        int value = 0;
+        
+        while(head!=NULL){
+            value = value + multiplier * head->val;
+            multiplier = multiplier * 2;
+            head = head->next;
+        }
+        
+        return value;
+    }
+};
