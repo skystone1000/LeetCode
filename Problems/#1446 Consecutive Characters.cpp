3ad47543852f71bf3855652da7b05592928e6653
Aditya <adityaspmahajan@gmail.com>
2021-12-13 11:13:23 +0530

    1446. Consecutive Characters

--- Problems/#1446 Consecutive Characters.cpp
+++ Problems/#1446 Consecutive Characters.cpp
@@ -1,4 +1,35 @@
 // 1446 Consecutive Characters
+// https://leetcode.com/problems/consecutive-characters/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxPower(string s) {
+        if(s.size() == 0){
+            return 0;
+        }
+        
+        int power = 1;
+        int currMax = 1;
+        for(int i=1;i<s.size();i++){
+            if(s[i-1] == s[i]){
+                currMax++;
+            }
+            
+            // Not doing else so that we can get last iteration check
+            // Sample input = "cc"
+            if(s[i-1] != s[i] || i == s.size()-1){
+                if(currMax > power)
+                    power = currMax;
+                currMax = 1;
+            }
+        }
+        
+        return power;
+    }
+};
 
 class Solution {
 public:
