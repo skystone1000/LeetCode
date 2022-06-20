97b71fb0a28eecc0f41c1081211c96f6d5e938fd
Aditya <adityaspmahajan@gmail.com>
2022-06-20 22:14:39 +0530

    Arrays Revision

--- Problems/#0977 Squares of a Sorted Array.cpp
+++ Problems/#0977 Squares of a Sorted Array.cpp
@@ -5,7 +5,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+// Method 1 - Time O(N)
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        stack<int> st;
+        int back = nums.size()-1, front = 0;
+        
+        while(front <= back){
+            // cout<<front<<" "<<back<<endl;
+            if(llabs(nums[front]) > llabs(nums[back])){
+                st.push(nums[front] * nums[front]);
+                front++;
+            }else{
+                st.push(nums[back] * nums[back]);                
+                back--;
+            }
+            // cout<<st.top()<<endl;
+        }
+        
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
 
 // Method 2 - Time O(NlogN) 
 class Solution {
--- Problems/#1089 Duplicate Zeros.cpp
+++ Problems/#1089 Duplicate Zeros.cpp
@@ -1,31 +1,40 @@
 // LeetCode arrays 4
 // #1089 Duplicate Zeros
+// https://leetcode.com/problems/duplicate-zeros/
 
+#include<bits/stdc++.h>
+using namespace std;
 
+// Method 2 (Creating new array)
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        /*
-        // Method 1 (TLE)
+        vector<int> ans;
+        for(int i=0;i<arr.size();i++){
+            if(ans.size() == arr.size())
+                break;
+            if(arr[i] == 0){
+                ans.push_back(0);
+                ans.push_back(0);
+            } else {
+                ans.push_back(arr[i]);
+            }
+        }
+        
+        for(int i=0;i<arr.size();i++){
+            arr[i] = ans[i];
+        }
+    }
+};
+
+// Method 1 (TLE)
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
         for(int i=0;i<arr.size();i++){
             if(arr[i] == 0){
                 arr.insert( arr.begin() + i , 0 );
             }
-        }*/
-        
-        // Method 2
-        vector<int> newArr;
-        newArr.reserve(arr.size());
-        for(int i=0;i<arr.size();i++){
-            if(arr[i] == 0){
-                newArr.push_back(0);
-            }
-            newArr.push_back(arr[i]);
-        }
-        
-        arr.reserve(newArr.size());
-        for(int i=0;i<newArr.size();i++){
-            arr[i] = newArr[i];
         }
     }
 };
