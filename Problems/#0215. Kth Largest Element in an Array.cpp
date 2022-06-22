d4768a040eabcd342d62348789bae7fb18d88dba
Aditya <adityaspmahajan@gmail.com>
2022-06-22 10:24:00 +0530

    LC - 215

---
+++ Problems/#0215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,37 @@
+// #0215. Kth Largest Element in an Array
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Method 2 - Max heap
+// Time - 8 ms, Space - 10.5 MB
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // Max Heap
+        priority_queue<int> q;
+        for(int i=0;i<nums.size();i++){
+            q.push(nums[i]);
+        }
+        for(int i=0;i<k;i++){
+            if(i==k-1)
+                return q.top();
+            else
+                q.pop();
+        }
+        return -1;
+    }
+};
+
+// Method 2 - Sorting
+// 	Time - 18 ms, Space - 10.1 MB
+/*
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-k];
+    }
+};
+*/
