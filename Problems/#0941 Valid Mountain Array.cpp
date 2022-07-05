89a096f4d9611d50e307126e8694ca6e04dae0c6
Aditya <adityaspmahajan@gmail.com>
2022-07-06 00:24:55 +0530

    Arrays Revision

--- Problems/#0941 Valid Mountain Array.cpp
+++ Problems/#0941 Valid Mountain Array.cpp
@@ -1,14 +1,32 @@
 // LeetCode arrays 9
 // #0941 Valid Mountain Array  
+// https://leetcode.com/problems/valid-mountain-array/submissions/
 
+#include<bits/stdc++.h>
+using namespace std;
+
+// Method 1 - Two pointer start, end
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        int n = A.size(), i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) 
+            i++;
+        while (j > 0 && A[j - 1] > A[j]) 
+            j--;
+        
+        if(i > 0 && i == j && j < n - 1)
+            return true;
+        return false;
+    }
+};
+
+// Method 2
 class Solution {
 public:
     bool validMountainArray(vector<int>& A) {
         int n = A.size();
-        
-        if(n < 3){
-            return false;
-        }
+        if(n < 3) return false;
         
         // Find max;
         int max = 0;
@@ -21,34 +39,23 @@ public:
         }
         
         // Check if only increasing or decreasing
-        if(index == 0 || index == n-1){
+        if(index == 0 || index == n-1)
             return false;
-        }
-        
         
         // Check Increasing
-        // int flag = 0 ;
         for(int i=1;i<=index;i++){
-            if(A[i] > A[i-1]){
+            if(A[i] > A[i-1])
                 continue;
-            }else{
-                // flag = 1;
-                // break;
+            else
                 return false;
-            }
         }
-        
         // Check Decreasing
         for(int i=index;i<n-1;i++){
-            if(A[i] > A[i+1]){
+            if(A[i] > A[i+1])
                 continue;
-            }else{
-                // flag = 1;
-                // break;
+            else
                 return false;
-            }
         }
-        
         return true;
         
     }
--- Problems/#1051 Height Checker.cpp
+++ Problems/#1051 Height Checker.cpp
@@ -1,25 +1,43 @@
 // LeetCode arrays 15
 // #1051 Height Checker
+// https://leetcode.com/problems/height-checker/
 
+#include<bits/stdc++.h>
+using namespace std;
+
+// Method 1 - Sorting
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> sortedStd;
-        //sortedStd.reserve(heights.size());
+        vector<int> expected = heights;
+        sort(expected.begin(),expected.end());
+        int ans=0;
+        for(int i=0;i<expected.size();i++){
+            if(expected[i] != heights[i])
+                ans++;
+        }
         
+        return ans;
+    }
+};
+
+// method 2 - Frequency array
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> heightFreq(101,0);
+        
+        for(int height:heights)
+            heightFreq[height]++;
+            
+        int result=0, currHeight=0;
         for(int i=0;i<heights.size();i++){
-            sortedStd.push_back(heights[i]);
+            while(heightFreq[currHeight] == 0)
+                currHeight++;
+            if(currHeight != heights[i])
+                result++;
+            heightFreq[currHeight]--;
         }
-        sort(sortedStd.begin(),sortedStd.end());
-        
-        int changedCount = 0;
-        for(int i=0;i<sortedStd.size();i++){
-            cout<<sortedStd[i]<<"="<<heights[i];
-            if(sortedStd[i] != heights[i]){
-                changedCount++;
-            }
-        }
-        
-        return changedCount;
+        return result;
     }
 };
