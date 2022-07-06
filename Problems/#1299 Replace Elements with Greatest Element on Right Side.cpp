e9a3b94eea675f294b8bca10a5abc9d1dfb50bce
Aditya <adityaspmahajan@gmail.com>
2022-07-07 00:13:51 +0530

    Arrays complete

--- Problems/#1299 Replace Elements with Greatest Element on Right Side.cpp
+++ Problems/#1299 Replace Elements with Greatest Element on Right Side.cpp
@@ -1,20 +1,39 @@
 // LeetCode arrays 10
 // #1299 Replace Elements with Greatest Element on Right Side  
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
 
+#include<bits/stdc++.h>
+using namespace std;
+
+// Method 1 - Brute Force 
+// Time - O(n^2)
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        for(int i=0;i<arr.size();i++){
-            int max = 0;
+        for(int i=0;i<arr.size()-1;i++){
+            int maxCurr = INT_MIN;
             for(int j=i+1;j<arr.size();j++){
-                if(max < arr[j]){
-                    max = arr[j];
-                }
+                if(arr[j] > maxCurr)
+                    maxCurr = arr[j];
             }
-            arr[i] = max;
-            if(i == arr.size()-1){
-                arr[i] = -1;
-            }
+            arr[i] = maxCurr;
+        }
+        arr[arr.size()-1] = -1;
+        return arr;
+    }
+};
+
+// Method 2 - Reverse traversal (inplace)
+// Time - O(n)
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int maxCurr = -1;
+        for(int i=arr.size()-1;i>=0;i--){
+            int currEle = arr[i];
+            arr[i] = maxCurr;
+            if(maxCurr < currEle)
+                maxCurr = currEle;
         }
         return arr;
     }
--- Problems/#1346 Check If N and Its Double Exist.cpp
+++ Problems/#1346 Check If N and Its Double Exist.cpp
@@ -1,15 +1,46 @@
 // LeetCode arrays 8
 // #1346 Check If N and Its Double Exist    
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/
 
+#include<bits/stdc++.h>
+using namespace std;
+
+// Method 1 - Brute Force
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr.size();j++){
+                if(i==j) continue;
+                if(arr[i] == 2*arr[j] || arr[j] == 2*arr[i])
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
+// Method 2 - Hashset
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        set<int> check;
+        for(int ele:arr){
+            if(check.count(2*ele) > 0 || (ele%2==0 && check.count(ele/2) > 0 ))
+                return true;
+            check.insert(ele);
+        }
+        return false;
+    }
+};
+
+// Method - Alternative for method 1
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
         int flag = 0;
         std::vector<int>::iterator it;
-        
         for(int element:arr){
-            
             // Handling case of zero
             if(element == 0){
                 int count = 0;
