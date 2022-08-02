3732f8fd42a7779b258e136abe8b286b92a43940
Aditya <adityaspmahajan@gmail.com>
2022-08-02 18:21:53 +0530

    Arrays Card - all ques all menthods

--- Problems/#1089 Duplicate Zeros.cpp
+++ Problems/#1089 Duplicate Zeros.cpp
@@ -5,6 +5,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Method 3 (Time - O(n), Space - O(1)) - two pass
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int i=0;
+        int temp = 0;
+        bool edgeZero = false;
+        
+        // Finding new End point
+        while(temp<arr.size()){
+            if(arr[i] == 0){
+                i++;
+                if(temp == arr.size()-1){
+                    edgeZero = true;
+                    temp++;
+                }else{
+                    temp += 2;
+                }
+            } else {
+                i++;
+                temp++;
+            }
+        }
+        
+        // Hadling Edge case if only one zero at end
+        i--;temp--;
+        if(edgeZero == true){
+            arr[temp] = arr[i];
+            temp--;
+            i--;
+        }
+        
+        // Updating elements
+        while(i>=0){
+            if(arr[i] == 0){
+                arr[temp] = 0;
+                arr[temp-1] = 0;
+                temp -= 2;
+                i--;
+            }else{
+                arr[temp] = arr[i];
+                i--;temp--;
+            }
+        }
+        
+    }
+};
+
 // Method 2 (Creating new array)
 class Solution {
 public:
