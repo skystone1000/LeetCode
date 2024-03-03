1033e85766a34f3c2e65ad74149474b2554ebbe9
Aditya <adityaspmahajan@gmail.com>
2024-03-03 12:00:25 +0530

    #0123. Best Time to Buy and Sell Stock III.cpp

---
+++ Problems/#0123. Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,64 @@
+// #0123. Best Time to Buy and Sell Stock III
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Approach 2: One-pass Simulation
+// Time complexity : O(N), Space complexity : O(1)
+class Solution {
+  public:
+  int maxProfit(vector<int>& prices) {
+    int t1Cost = INT_MAX, 
+        t2Cost = INT_MAX;
+    int t1Profit = 0,
+        t2Profit = 0;
+
+    for (int price : prices) {
+        // the maximum profit if only one transaction is allowed
+        t1Cost = min(t1Cost, price);
+        t1Profit = max(t1Profit, price - t1Cost);
+        // re-invest the gained profit in the second transaction
+        t2Cost = min(t2Cost, price - t1Profit);
+        t2Profit = max(t2Profit, price - t2Cost);
+    }
+
+    return t2Profit;
+  }
+};
+
+
+// Approach 1: Bidirectional Dynamic Programming
+// Time complexity : O(N), Space complexity : O(N)
+class Solution {
+  public:
+  int maxProfit(vector<int>& prices) {
+    int length = prices.size();
+    if (length <= 1) return 0;
+
+    int leftMin = prices[0];
+    int rightMax = prices[length - 1];
+
+    vector<int> leftProfits(length, 0);
+    // pad the right DP array with an additional zero for convenience.
+    vector<int> rightProfits(length + 1, 0);
+
+    // construct the bidirectional DP array
+    for (int l = 1; l < length; ++l) {
+      leftProfits[l] = max(leftProfits[l - 1], prices[l] - leftMin);
+      leftMin = min(leftMin, prices[l]);
+
+      int r = length - 1 - l;
+      rightProfits[r] = max(rightProfits[r + 1], rightMax - prices[r]);
+      rightMax = max(rightMax, prices[r]);
+    }
+
+    int maxProfit = 0;
+    for (int i = 0; i < length; ++i) {
+      maxProfit = max(maxProfit, leftProfits[i] + rightProfits[i + 1]);
+    }
+    return maxProfit;
+  }
+};
